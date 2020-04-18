d4a2ae7bcef60aa736db0d12c8235ab8c1ded35d
shahin-shuvo <shahinshuvo96@gmail.com>
2020-04-19 02:15:59 +0600

    Version4

---
+++ build/precache-manifest.03dae850947a71a90e5c5884707932af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a22de84a53111fc1e6fb2dbbca3748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421754798d56ab68f8f7",
+    "url": "/static/css/main.a2a78fe9.chunk.css"
+  },
+  {
+    "revision": "9d5be34b35d8e2ce14a5",
+    "url": "/static/js/2.689dfd52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.689dfd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421754798d56ab68f8f7",
+    "url": "/static/js/main.37df9a66.chunk.js"
+  },
+  {
+    "revision": "fba7532ceb00c65e164f",
+    "url": "/static/js/runtime-main.86eac344.js"
+  }
+]);
